329996b2333710c7fa3cc9b2cb531c0f26d3ca17
Morten Nissen <morten@m9ssen.me>
2024-03-15 14:18:05 +0100

    Now registers shortcut via keyboardServices

--- Src/DFFWizard.pas
+++ Src/DFFWizard.pas
@@ -11,21 +11,28 @@ type
 
 
 
-  TDFFWizard = class(TNotifierObject, IOTAWizard)
+  TDFFWizard = class(TNotifierObject, IOTAWizard, IOTAKeyboardBinding)
   private
     FEvents: TApplicationEvents;
 
-    procedure DoApplicationMessage(var Msg: TMsg; var Handled: Boolean);
+    function GetBindingType: TBindingType;
+    function GetDisplayName: string;
 
-  protected
-    procedure EditKeyUp(Key, ScanCode: Word; Shift: TShiftState; Msg: TMsg; var Handled: Boolean);
+    procedure ShowForm(const Context: IOTAKeyContext; KeyCode: TShortcut;
+    var BindingResult: TKeyBindingResult);
   public
     constructor Create;
 
     function GetIDString:string;
     function GetName: string;
     function GetState: TWizardState;
+
     procedure Execute;
+
+    procedure BindKeyboard(const BindingServices: IOTAKeyBindingServices);
+
+    property BindingType: TBindingType read GetBindingType;
+    property DisplayName: string read GetDisplayName;
   end;
 
 var
@@ -35,103 +42,33 @@ implementation
 
 uses
   DFFFilesForm, System.IOUtils,
-  Vcl.Controls;
+  Vcl.Controls, Vcl.Menus;
 
 { TDFFWizard }
 
+procedure TDFFWizard.BindKeyboard(
+  const BindingServices: IOTAKeyBindingServices);
+begin
+  BindingServices.AddKeyBinding([ShortCut(Ord('T'), [ssCtrl])], ShowForm, nil, 0, '', 'Nogen file finder');
+end;
+
 constructor TDFFWizard.Create;
 begin
-  FEvents := TApplicationEvents.Create(nil);
-  FEvents.OnMessage := DoApplicationMessage;
-end;
-
-procedure TDFFWizard.DoApplicationMessage(var Msg: TMsg; var Handled: Boolean);
-var
-  Key: Word;
-  ScanCode: Word;
-  Shift: TShiftState;
-begin
-  if ((Msg.message = WM_KEYDOWN) or (Msg.message = WM_KEYUP)) then
-  begin
-    Key := Msg.wParam;
-    ScanCode := (Msg.lParam and $00FF0000) shr 16;
-    Shift := KeyDataToShiftState(Msg.lParam);
-
-    if Key = VK_PROCESSKEY then
-    begin
-      Key := MapVirtualKey(ScanCode, 1);
-    end;
-
-    if Msg.message = WM_KEYUP then
-    begin
-      EditKeyUp(Key, ScanCode, Shift, Msg, Handled);
-    end;
-  end;
-end;
-
-procedure TDFFWizard.EditKeyUp(Key, ScanCode: Word; Shift: TShiftState;
-  Msg: TMsg; var Handled: Boolean);
-begin
-  var lCtrl:Boolean := (GetKeyState(VK_CONTROL) < 0);
-  if lCtrl and (Chr(Key) = 'T') then
-  begin
-
-    //TODO: The finding and ranking of files should be done in a BG thread,
-    // so all that needs to happen when they press the button is showing the GUI
-    var lProject := GetActiveProject;
-
-    var lFiles := TStringList.Create;
-    lFiles.OwnsObjects := True;
-
-    var lTmpFiles := TStringList.Create;
-    lProject.GetCompleteFileList(lTmpFiles);
-
-    for var lFile in lTmpFiles do
-    begin
-      lFiles.AddObject(TPath.GetFileNameWithoutExtension(lFile), TStrObj.Create(lFile));
-    end;
-
-    var lSearchUnits := TStringList.Create;
-    var lConf := lProject.ProjectOptions as IOTAProjectOptionsConfigurations;
-
-    var lPlatform := lConf.ActiveConfiguration;
-    lPlatform.GetValues('DCC_UnitSearchPath', lSearchUnits);
-
-    for var lSearchUnit in lSearchUnits do
-    begin
-      var lPath :TFileName;
-      var lFolder := ExtractFilePath(lProject.FileName);
-
-      lPath := TPath.GetFullPath(TPath.Combine(lFolder, lSearchUnit));
-
-      if TDirectory.Exists(lPath) then
-      begin
-        for var lFile in TDirectory.GetFiles(lPath, '*.pas') do
-        begin
-          lFiles.AddObject(TPath.GetFileNameWithoutExtension(lFile), TStrObj.Create(lFile));
-        end;
-      end;
-    end;
-
-    var lForm := TfrmDFFFiles.Create(nil);
-    lForm.SetFiles(lFiles);
-    if lForm.ShowModal = mrOK then
-    begin
-      var lActionService :IOTAActionServices;
-      if Supports(BorlandIDEServices, IOTAActionServices, lActionService) then
-      begin
-        lActionService.OpenFile(lForm.SelectedFile);
-      end;
-    end;
-    FreeAndNil(lForm);
-    FreeAndNil(lFiles);
-    Handled := True;
-  end;
 end;
 
 procedure TDFFWizard.Execute;
 begin
 
+end;
+
+function TDFFWizard.GetBindingType: TBindingType;
+begin
+  Result := TBindingType.btPartial;
+end;
+
+function TDFFWizard.GetDisplayName: string;
+begin
+  Result := GetName;
 end;
 
 function TDFFWizard.GetIDString: string;
@@ -147,6 +84,60 @@ end;
 function TDFFWizard.GetState: TWizardState;
 begin
   Result := [wsEnabled];
+end;
+
+procedure TDFFWizard.ShowForm(const Context: IOTAKeyContext; KeyCode: TShortcut;
+    var BindingResult: TKeyBindingResult);
+begin
+  //TODO: The finding and ranking of files should be done in a BG thread,
+  // so all that needs to happen when they press the button is showing the GUI
+  var lProject := GetActiveProject;
+
+  var lFiles := TStringList.Create;
+  lFiles.OwnsObjects := True;
+
+  var lTmpFiles := TStringList.Create;
+  lProject.GetCompleteFileList(lTmpFiles);
+
+  for var lFile in lTmpFiles do
+  begin
+    lFiles.AddObject(TPath.GetFileNameWithoutExtension(lFile), TStrObj.Create(lFile));
+  end;
+
+  var lSearchUnits := TStringList.Create;
+  var lConf := lProject.ProjectOptions as IOTAProjectOptionsConfigurations;
+
+  var lPlatform := lConf.ActiveConfiguration;
+  lPlatform.GetValues('DCC_UnitSearchPath', lSearchUnits);
+
+  for var lSearchUnit in lSearchUnits do
+  begin
+    var lPath :TFileName;
+    var lFolder := ExtractFilePath(lProject.FileName);
+
+    lPath := TPath.GetFullPath(TPath.Combine(lFolder, lSearchUnit));
+
+    if TDirectory.Exists(lPath) then
+    begin
+      for var lFile in TDirectory.GetFiles(lPath, '*.pas') do
+      begin
+        lFiles.AddObject(TPath.GetFileNameWithoutExtension(lFile), TStrObj.Create(lFile));
+      end;
+    end;
+  end;
+
+  var lForm := TfrmDFFFiles.Create(nil);
+  lForm.SetFiles(lFiles);
+  if lForm.ShowModal = mrOK then
+  begin
+    var lActionService :IOTAActionServices;
+    if Supports(BorlandIDEServices, IOTAActionServices, lActionService) then
+    begin
+      lActionService.OpenFile(lForm.SelectedFile);
+    end;
+  end;
+  FreeAndNil(lForm);
+  FreeAndNil(lFiles);
 end;
 
 end.
--- Src/DLLEntry.pas
+++ Src/DLLEntry.pas
@@ -33,6 +33,15 @@ begin
   end;
 end;
 
+procedure Registerkeybinds(const BorlandIDEServices: IBorlandIDEServices; aDFFWiz: TDFFWizard);
+begin
+  var keyboardBinding: IOTAKeyboardServices;
+  if Supports(BorlandIDEServices, IOTAKeyboardServices, keyboardBinding) then
+  begin
+    keyboardBinding.AddKeyboardBinding(aDFFWiz);
+  end;
+end;
+
 
 function InitWizard(const BorlandIDEServices: IBorlandIDEServices;
   RegisterProc: TWizardRegisterProc; var Terminate: TWizardTerminateProc): Boolean; stdcall;
@@ -47,6 +56,10 @@ begin
     WizardServices := BorlandIDEServices as IOTAWizardServices;
     Assert(Assigned(WizardServices));
     DFFWiz := TDFFWizard.Create;
+
+    RegisterKeyBinds(BorlandIDEServices, DFFWiz);
+
+
     FWizardIndex := WizardServices.AddWizard(DFFWiz as IOTAWizard);
     Result := (FWizardIndex >= 0);
   end;
