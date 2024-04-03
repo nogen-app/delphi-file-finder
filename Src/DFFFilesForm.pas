991d7d167509ecf66f19e0f18d8842916fbf5eb3
Morten Nissen <morten@m9ssen.me>
2024-04-03 08:40:30 +0200

    Added Alexandria package

--- Src/DFFFilesForm.pas
+++ Src/DFFFilesForm.pas
@@ -5,7 +5,7 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
-  Vcl.Samples.Spin, Vcl.FileCtrl, ToolsAPI, ToolsAPI.UI,
+  Vcl.Samples.Spin, Vcl.FileCtrl, ToolsAPI,
   DockableForm, FilesFrame;
 
 type
